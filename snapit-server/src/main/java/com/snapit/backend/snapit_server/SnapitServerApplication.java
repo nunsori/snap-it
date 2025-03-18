f7bab40d08a3878f07eee53d05c2758cbee1a6f9
chabin37 <7chabin@gmail.com>
2025-03-19 00:51:42 +0900

    Initial commit

---
+++ snapit-server/src/main/java/com/snapit/backend/snapit_server/SnapitServerApplication.java
@@ -0,0 +1,13 @@
+package com.snapit.backend.snapit_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapitServerApplication.class, args);
+	}
+
+}
---
+++ snapit-server/src/test/java/com/snapit/backend/snapit_server/SnapitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapit.backend.snapit_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
